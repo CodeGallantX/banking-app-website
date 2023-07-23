e2d40a03295253e9f7a76fe68622e931e6f8b400
Ayodele Bankole <bankoleheebrahymn@gmail.com>
2023-07-23 13:49:45 +0100

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Stats from './Stats';
+import Business from './Business';
+import Billing from './Billing';
+import CardDeal from './CardDeal';
+import Testimonials from './Testimonials';
+import CTA from './CTA';
+import Footer from './Footer';
+import Customers from './Customers'
+
+
+
+
+
+export {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  CTA,
+  Customers,
+  Footer
+  
+ 
+};
